9d1919d30367f4bb3efffcfdac8aecfb5c184805
Armano-Castillo <armandocreyes02@gmail.com>
2020-09-25 02:54:30 -0500

    BinarySearchTree algoritmo

--- tree.c
+++ tree.c
@@ -7,11 +7,32 @@ typedef struct tree{
     struct tree *pointer2;
 } tree;
 
+typedef struct trie{
+    char *name[10];
+    struct trie *pointers[10];
+} trie;
+
+//Prototype
+void binarySearchTree(tree *arbol, int n);
+
 int main(void){
 
 }
 
 
 void binarySearchTree(tree *arbol, int n){
-    
+    tree *tmp = arbol;
+    while(tmp != NULL){
+        if ( (n) < (tmp -> value)){
+            tmp = tmp -> pointer1;
+        }
+        else if(n > tmp -> value){
+            tmp = tmp -> pointer2;
+        }
+        else if(n == tmp -> value){
+            return "Encontrado";
+        } else {
+            return "No existe";
+        }
+   }
 }
